a715691194c569efe3f1f4ad64c08ab9b5833e6c
ARFAT-HASAAN <arfuhtv757@gmail.com>
2021-09-02 19:27:05 +0600

    third commit

--- script.js
+++ script.js
@@ -1,9 +1,67 @@
+const resultTotal = document.getElementById('result-total')
+resultTotal.style.display = 'none'
+
+
 const searchBookName = () => {
     const inputField = document.getElementById('input-field');
     const inputValue = inputField.value;
-    // console.log(inputValue);
+    inputField.value = '';
+    resultTotal.style.display = "none";
+    bookContainer.textContent = '';
+
     const url = `https://openlibrary.org/search.json?q=${inputValue}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => bookDetails(data))
 }
+
+const bookDetails = details => {
+    // console.log(details)
+    const resultTotal = document.getElementById('result-total');
+    resultTotal.style.display = 'block'
+    resultTotal.textContent = '';
+    const p = document.createElement('p')
+    if (details.numFound === 0) {
+        p.innerText = `Apni je namer book kojtesen oidoroner kono voi amder kase nei`
+        resultTotal.appendChild(p);
+    }
+    else {
+        p.innerText = `You have found ${details.numFound} result`
+        resultTotal.appendChild(p);
+    }
+
+    details.docs.forEach(livrary => {
+        foundBooks(livrary)
+    })
+
+}
+
+const bookContainer = document.getElementById('book-Container')
+// bookContainer.textContent = '';
+
+const foundBooks = book => {
+    console.log(book)
+
+    const url = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+    console.log(url);
+    const div = document.createElement('div')
+    div.classList.add('col')
+
+    div.innerHTML = `
+                <div class="card">
+                <img src="${url}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h6 class="">Books Name:<span class="fw-normal"> ${book.title} </span></h6>
+                    <h6 class="">Author Name:<span class="fw-normal"> ${book.author_name} </span></h6>
+                    <h6 class="">Publisher Name:<span class="fw-normal"> ${book.publisher} </span></h6>
+                    <h6 class="">First publish date:<span class="fw-normal"> ${book.first_publish_year} </span></h6>
+                </div>
+            </div>
+        `
+    bookContainer.appendChild(div)
+}
+
+
+
+
+
