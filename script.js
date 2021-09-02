d062a09f38364f6e1d25256baf484a078b711416
ARFAT-HASAAN <arfuhtv757@gmail.com>
2021-09-02 10:32:08 +0600

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hi');
+const searchBookName = () => {
+    const inputField = document.getElementById('input-field');
+    const inputValue = inputField.value;
+    // console.log(inputValue);
+    const url = `https://openlibrary.org/search.json?q=${inputValue}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
