80547dfbd760b0aa453a73f2ae3d1ff7cf9c50e6
ARFAT-HASAAN <arfuhtv757@gmail.com>
2021-09-02 22:22:15 +0600

    final commit

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
+// error section hided 
 const resultTotal = document.getElementById('result-total')
 resultTotal.style.display = 'none'
 
+// book container id 
+const bookContainer = document.getElementById('book-Container')
 
+// search book  by name with function  
 const searchBookName = () => {
     const inputField = document.getElementById('input-field');
     const inputValue = inputField.value;
     inputField.value = '';
+
+    // error section hided second time 
     resultTotal.style.display = "none";
+
+    // clear book container 
     bookContainer.textContent = '';
 
+    // book name url 
     const url = `https://openlibrary.org/search.json?q=${inputValue}`
     fetch(url)
         .then(res => res.json())
         .then(data => bookDetails(data))
 }
 
+// capture books details 
 const bookDetails = details => {
-    // console.log(details)
+
+    // arror section showing 
     const resultTotal = document.getElementById('result-total');
     resultTotal.style.display = 'block'
     resultTotal.textContent = '';
+
+    // arror secton condition 
     const p = document.createElement('p')
     if (details.numFound === 0) {
-        p.innerText = `Apni je namer book kojtesen oidoroner kono voi amder kase nei`
+        p.innerText = `no result found`
         resultTotal.appendChild(p);
     }
     else {
@@ -30,24 +43,25 @@ const bookDetails = details => {
         resultTotal.appendChild(p);
     }
 
-    details.docs.forEach(livrary => {
-        foundBooks(livrary)
-    })
-
+    // splcie books array 
+    const splice = details.docs.splice(0, 20)
+    // calling foundbook function 
+    foundBooks(splice)
 }
 
-const bookContainer = document.getElementById('book-Container')
-// bookContainer.textContent = '';
+// foundbook functon 
+const foundBooks = books => {
 
-const foundBooks = book => {
-    console.log(book)
+    // apply forEach on books aray 
+    books.forEach(book => {
 
-    const url = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
-    console.log(url);
-    const div = document.createElement('div')
-    div.classList.add('col')
+        // books photos url 
+        const url = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
 
-    div.innerHTML = `
+        // create div for books detaisl 
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `
                 <div class="card">
                 <img src="${url}" class="card-img-top" alt="">
                 <div class="card-body">
@@ -58,7 +72,8 @@ const foundBooks = book => {
                 </div>
             </div>
         `
-    bookContainer.appendChild(div)
+        bookContainer.appendChild(div)
+    })
 }
 
 
